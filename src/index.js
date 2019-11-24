f780c6d5a1571ccdbf84c83318dfd989be2d42ff
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2019-11-24 23:00:48 +0000

    Load the three datasets asynchronously; Create parser functions for the elections and fires datasets;

--- src/index.js
+++ src/index.js
@@ -3,19 +3,29 @@ import './components/selections'
 
 import * as d3 from 'd3'
 import companiesCSV from './datasets/companies_dataset.csv'
+import electionsCSV from './datasets/elections_dataset.csv'
+import firesCSV from './datasets/fires_dataset.csv'
 import CompaniesProductivityComponent from './components/companies_productivity'
 import ElectionsComponent from './components/elections'
 //
 //
 ;(async () => {
-  const companiesData = await d3.csv(companiesCSV, parseCompaniesData)
+  const [companiesData, electionsData, firesData] = await Promise.all([
+    d3.csv(companiesCSV, parseCompaniesData),
+    d3.csv(electionsCSV, parseElectionsData),
+    d3.csv(firesCSV, parseFiresData)
+  ])
 
   const defaultMunicipality = 'Continente'
   const containerSelector = '.divergent-charts-section'
   const components = []
 
   const mainSectionWidth = document.querySelector(containerSelector).offsetWidth
-  const dispatch = registerEventListeners(companiesData)
+  const dispatch = registerEventListeners({
+    companiesData,
+    electionsData,
+    firesData
+  })
 
   // Create all the components of the dashboard
   components.push(
@@ -31,7 +41,7 @@ import ElectionsComponent from './components/elections'
       dispatch,
       containerSelector,
       mainSectionWidth
-    )
+  )
   )
 
   // Initialize dashboard components
@@ -43,7 +53,7 @@ import ElectionsComponent from './components/elections'
   )
 })()
 
-function registerEventListeners (fullDataset) {
+function registerEventListeners ({ companiesData: fullDataset }) {
   const dispatch = d3.dispatch(
     'initialize',
     'update_municipality',
@@ -85,5 +95,31 @@ function parseCompaniesData (datum) {
     type: datum['Company Type'],
     number: +datum['Number of Companies'],
     productivity: +datum.Productivity
+  }
+}
+
+function parseElectionsData (datum) {
+  return {
+    nuts: datum.NUTS,
+    location: datum.Location,
+    year: +datum.Year,
+    type: datum['Election Type'],
+    PS: +datum.PS,
+    PSD: +datum.PSD,
+    CDS: +datum.CDS,
+    PCP: +datum.PCP,
+    BE: +datum.BE,
+    abstention: +datum.Abstention
+  }
+}
+
+function parseFiresData (datum) {
+  return {
+    nuts: datum.NUTS,
+    location: datum.Location,
+    year: +datum.Year,
+    fires: +datum.Fires,
+    firefighters: +datum.Firefighters,
+    tourism: +datum.Tourism
   }
 }
