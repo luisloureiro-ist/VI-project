cdc1609c721ebb0989e6280edcb624c4db8d7a7b
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2019-11-26 01:02:33 +0000

    Prepare idiom to update when years change.

--- src/idioms/cleveland_dot_plots/index.js
+++ src/idioms/cleveland_dot_plots/index.js
@@ -72,12 +72,76 @@ class ClevelandDotPlots {
     this.__createLegend(categories)
   }
 
+  update (data, categories, chartTitle) {
+    if (this.yAxisPadding !== 0) {
+      this.__updateYAxis(data)
+    }
+    this.__updateXAxis(data)
+    this.__updateLegend(categories)
+
+    this.xScaler
+      .domain([0, d3.max(data, d => Math.max(...d.results)) + 2])
+      .range([0, this.chartWidth - this.yAxisPadding])
+    this.yScaler = d3
+      .scaleBand()
+      .range([0, 2 * this.dotRadius * (data.length + 1)])
+      .domain(data.map(d => d.key))
+
+    this.__setChartHeight(data.length)
+    this.chart.attr(
+      'height',
+      3 * this.dotRadius +
+        this.dotRadius * 2 * (data.length + 1) +
+        this.dotRadius * 2 +
+        35
+    )
+
+    this.chart
+      .select('.clevelands')
+      .selectAll('.cleveland')
+      .data(data)
+      .join(
+        enter => {
+          const lineAndCirclesGroup = enter
+            .append('g')
+            // .attr('transform', this.__getTranslate.bind(this))
+            .classed('cleveland', true)
+
+          data[0].results.forEach((result, idx) => {
+            lineAndCirclesGroup.call(this.__createDot.bind(this, idx))
+          })
+        },
+        update =>
+          update
+            // .attr('transform', this.__getTranslate.bind(this))
+            .call(this.__updateDot.bind(this)),
+        exit => exit.remove()
+      )
+
+    this.__updateXAxis(data)
+  }
+
   __createXAxis (data) {
     this.xAxis.scale(this.xScaler).tickSizeOuter(0) // suppresses the square ends of the domain path, instead producing a straight line.
 
     this.chart
       .append('g')
       .classed('x-axis', true)
+      .attr(
+        'transform',
+        `translate(${this.yAxisPadding}, ${3 * this.dotRadius +
+          this.dotRadius * 2 * (data.length + 1) +
+          8})` // +8 for padding
+      )
+      .transition(this.transition)
+      .call(this.xAxis)
+  }
+
+  __updateXAxis (data) {
+    this.xAxis.scale(this.xScaler).tickSizeOuter(0) // suppresses the square ends of the domain path, instead producing a straight line.
+
+    this.chart
+      .select('x-axis', true)
       .attr(
         'transform',
         `translate(${this.yAxisPadding}, ${3 * this.dotRadius +
@@ -98,6 +162,25 @@ class ClevelandDotPlots {
     this.chart
       .append('g')
       .classed('y-axis', true)
+      .attr(
+        'transform',
+        `translate(${this.yAxisPadding}, ${3 * this.dotRadius +
+          this.dotRadius +
+          2})` // +2 for padding
+      )
+      .transition(this.transition)
+      .call(this.yAxis)
+  }
+
+  __updateYAxis (domain) {
+    if (this.yAxis === null) {
+      this.yAxis = d3.axisLeft()
+    }
+
+    this.yAxis.scale(this.yScaler).tickSizeOuter(0) // suppresses the square ends of the domain path, instead producing a straight line.
+
+    this.chart
+      .select('y-axis', true)
       .attr(
         'transform',
         `translate(${this.yAxisPadding}, ${3 * this.dotRadius +
@@ -131,6 +214,29 @@ class ClevelandDotPlots {
     })
   }
 
+  __updateLegend (categories) {
+    const legend = this.chart
+      .append('g')
+      .classed('legend', true)
+      .attr('height', 30)
+      .attr('transform', `translate(${this.yAxisPadding},30)`)
+    categories.forEach((category, idx) => {
+      legend
+        .append('circle')
+        .attr('cx', 50 + idx * 50)
+        .attr('cy', 130)
+        .attr('r', 6)
+        .style('fill', this.colors[idx])
+      legend
+        .append('text')
+        .attr('x', 50 + idx * 50 + 12)
+        .attr('y', 131)
+        .text(category)
+        .style('font-size', '12px')
+        .attr('alignment-baseline', 'middle')
+    })
+  }
+
   __createDot (resultsIdx, lines) {
     lines
       .append('circle')
@@ -141,6 +247,19 @@ class ClevelandDotPlots {
       .style('fill', () => this.colors[resultsIdx])
       .selection()
       .append('title')
+      .text(d => d.results[resultsIdx])
+  }
+
+  __updateDot (resultsIdx, lines) {
+    lines
+      .select('circle')
+      .transition(this.transition)
+      .attr('cx', d => this.xScaler(d.results[resultsIdx]) + this.yAxisPadding)
+      .attr('cy', d => this.yScaler(d.key) + this.dotRadius / 2)
+      .attr('r', this.dotRadius)
+      .style('fill', () => this.colors[resultsIdx])
+      .selection()
+      .select('title')
       .text(d => d.results[resultsIdx])
   }
 }
